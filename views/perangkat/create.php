1f0aa3b8f7fa0a291861866c8b3abe8aff73511a
PanduMA <pandumuhamad4@gmail.com>
2018-09-21 15:46:31 +0700

    tambah close di modal

--- views/perangkat/create.php
+++ views/perangkat/create.php
@@ -10,11 +10,13 @@ $this->title = 'Tambah Perangkat';
 $this->params['breadcrumbs'][] = ['label' => 'Perangkats', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="perangkat-create" style="
-                                  padding:20px;
-                                  ">
-
-    <center><h3><?= Html::encode($this->title) ?></h3></center><br>
+<div
+    class="perangkat-create"
+    style="padding:20px;">
+    <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <center>
+        <h3><?= Html::encode($this->title) ?></h3>
+    </center><br>
 
     <?= $this->renderAjax('_form', [
         'model' => $model,
