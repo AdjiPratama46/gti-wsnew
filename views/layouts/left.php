9a30d7a305228844f6314e31b65c6465c25c4367
adji <adjipratama22@gmail.com>
2018-09-24 13:20:13 +0700

    nama user id

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -8,7 +8,7 @@
 
             </div>
             <div class="pull-left info">
-                <p><?= Yii::$app->user->identity->name ?></p>
+                <p id="namauser"><?= Yii::$app->user->identity->name ?></p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
