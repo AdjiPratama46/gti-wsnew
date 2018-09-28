2de8379e9f2afc9ed8941a7bbbc0e05acff2cc91
PanduMA <pandumuhamad4@gmail.com>
2018-09-28 12:47:16 +0700

    xx

--- models/DataSearch.php
+++ models/DataSearch.php
@@ -1,32 +1,28 @@
 <?php
-
 namespace app\models;
-
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Data;
 use app\models\Perangkat;
-
 /**
  * DataSearch represents the model behind the search form of `app\models\Data`.
  */
-
 class DataSearch extends Data
 {
     /**
      * {@inheritdoc}
      */
      public $perangkat;
+     public $pukul;
     public function rules()
     {
         return [
             [['id_data', 'kapasitas_baterai'], 'integer'],
-            [['id_perangkat', 'tgl', 'arah_angin', 'perangkat'], 'safe'],
+            [['id_perangkat', 'tgl', 'arah_angin', 'perangkat','pukul'], 'safe'],
             [['kelembaban', 'kecepatan_angin', 'curah_hujan', 'temperature'], 'number'],
         ];
     }
-
     /**
      * {@inheritdoc}
      */
@@ -35,7 +31,6 @@ class DataSearch extends Data
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
-
     /**
      * Creates data provider instance with search query applied
      *
@@ -45,35 +40,25 @@ class DataSearch extends Data
      */
     public function search($params)
     {
-
         //Menentukan QUERY
         $query=$this->querynya($params);
-
         // add conditions that should always apply here
-
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'pagination' => [ 'pageSize' => 10 ],
         ]);
-
         $dataProvider->sort->attributes['perangkat'] = [
             'asc' => ['perangkat.alias' => SORT_ASC],
             'desc' => ['perangkat.alias' => SORT_DESC],
         ];
-
         $dataProvider->sort->attributes['pukul'] = [
             'asc' => ['tgl' => SORT_ASC],
             'desc' => ['tgl' => SORT_DESC],
         ];
-
-
-
         $this->load($params);
-
         if (!$this->validate()) {
             return $dataProvider;
         }
-
         // grid filtering conditions
         $query->andFilterWhere([
             'id_data' => $this->id_data,
@@ -83,80 +68,49 @@ class DataSearch extends Data
             'temperature' => $this->temperature,
             'kapasitas_baterai' => $this->kapasitas_baterai,
         ]);
-
         $query->andFilterWhere(['like', 'id_perangkat', $this->id_perangkat])
             ->andFilterWhere(['like', 'arah_angin', $this->arah_angin])
             ->andFilterWhere(['like', 'tgl', $this->tgl])
             ->andFilterWhere(['like', 'time(tgl)', $this->pukul])
             ->andFilterWhere(['like', 'perangkat.alias', $this->perangkat]);
-
         return $dataProvider;
     }
-
     private function querynya($params){
       $perangk = Perangkat::find()->where(['id_owner'=>Yii::$app->user->identity->id])->one();
-
-      $this->load($params);
+        $this->load($params);
         if(empty($this->tgl) && empty($this->id_perangkat) && !empty($perangk->id)){
-
-            $query = Data::find()->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->andWhere(
+            $query = Data::find()
+            ->joinWith('perangkat')
+            ->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])
+            ->andWhere(
                 [
                   'between',
                   'tgl',
                   date('Y-m-d H:i:s', mktime(0, 0, 0, date('m'), date('d')-1, date('Y'))),
                   date('Y-m-d H:i:s', mktime(23, 59, 59, date('m'), date('d')-1, date('Y')))
-                  ])->andWhere(['id_perangkat' => $perangk->id])->orderBy(['tgl' => SORT_ASC]);
-
-
+                  ])
+            ->andWhere(['id_perangkat' => $perangk->id])->orderBy(['tgl' => SORT_ASC]);
         }
-
         elseif(!empty($this->tgl) && empty($this->id_perangkat) && !empty($perangk->id)){
-          $query = Data::find()->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->andWhere(
+          $query = Data::find()
+          ->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->andWhere(
               ['id_perangkat' => $perangk->id])->orderBy(['tgl' => SORT_ASC]);
         }
+        elseif(!empty($this->id_perangkat) && empty($this->tgl)   && !empty($perangk->id)){
+          $query = Data::find()
+          ->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->andWhere(
+              ['id_perangkat' => $perangk->id])->andWhere(
+                  [
+                    'between',
+                    'tgl',
+                    date('Y-m-d H:i:s', mktime(0, 0, 0, date('m'), date('d')-1, date('Y'))),
+                    date('Y-m-d H:i:s', mktime(23, 59, 59, date('m'), date('d')-1, date('Y')))
+                    ])->orderBy(['tgl' => SORT_ASC]);
+        }
         else{
-          $query = Data::find()->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->orderBy(['tgl' => SORT_ASC]);;
+          $query = Data::find()
+          ->joinWith('perangkat')->where(['perangkat.id_owner' =>Yii::$app->user->identity->id])->orderBy(['tgl' => SORT_ASC]);;
         }
-
         return $query;
-
-        // add conditions that should always apply here
-
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-            'pagination' => [ 'pageSize' => 10 ],
-        ]);
-
-        $dataProvider->sort->attributes['perangkat'] = [
-            // The tables are the ones our relation are configured to
-            // in my case they are prefixed with "tbl_"
-            'asc' => ['perangkat.alias' => SORT_ASC],
-            'desc' => ['perangkat.alias' => SORT_DESC],
-        ];
-
-        $this->load($params);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id_data' => $this->id_data,
-            'kelembaban' => $this->kelembaban,
-            'kecepatan_angin' => $this->kecepatan_angin,
-            'curah_hujan' => $this->curah_hujan,
-            'temperature' => $this->temperature,
-            'kapasitas_baterai' => $this->kapasitas_baterai,
-        ]);
-
-        $query->andFilterWhere(['like', 'id_perangkat', $this->id_perangkat])
-            ->andFilterWhere(['like', 'arah_angin', $this->arah_angin])
-            ->andFilterWhere(['like', 'tgl', $this->tgl])
-            ->andFilterWhere(['like', 'perangkat.alias', $this->perangkat]);
-
-        return $dataProvider;
     }
 }
