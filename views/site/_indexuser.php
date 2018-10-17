4b5b257b2e6f8892817eb72928678ede45428b9b
PanduMA <pandumuhamad4@gmail.com>
2018-10-17 10:36:55 +0700

    dash

--- views/site/_indexuser.php
+++ views/site/_indexuser.php
@@ -326,7 +326,7 @@ $this->registerJs("
         </div>
     </div>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="box box-solid box-success">
                 <div class="box-header with-border">
                     <div class="row">
@@ -403,7 +403,7 @@ $this->registerJs("
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <div class="box  box-solid box-danger" id="bx-dg">
                 <div class="box-header with-border">
                     <h3 class="box-title">Statistik Arah Angin</h3>
@@ -421,7 +421,7 @@ $this->registerJs("
                                 $jmlh = $pieh['jumlah'];
                                 $hasil[] = array($arah, 
                                 (int)$jmlh );
-                             }
+                            }
                             
                             echo Highcharts::widget([
                                     'scripts' => [
