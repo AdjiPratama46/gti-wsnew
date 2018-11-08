9c6a73a0e784149bf9462e08c64499ca9ac32c8c
adji <adjipratama22@gmail.com>
2018-11-08 12:42:43 +0700

    user

--- views/user/create.php
+++ views/user/create.php
@@ -7,14 +7,14 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Users */
 
-$this->title = 'Create Users';
+$this->title = 'Create User';
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="users-create">
     <div class="box-body">
     <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <center><h3>Tambah Users</h3></center>
+    <center><h3>Tambah User</h3></center><br>
         <?php $form = ActiveForm::begin(); ?>
         <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -38,7 +38,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-md-offset-4">
                     <div class="form-group">
                         <?= $form->field($model, 'password')->hiddenInput()->label(false) ?>
-                        <?= Html::submitButton('Save', ['class' => 'btn btn-block btn-success']) ?>
+                        <?= Html::submitButton('Simpan', ['class' => 'btn btn-block btn-success']) ?>
                     </div>
                 </div>
 
