7cc283465d551347ca69a7b1a2339ad420985fd6
adji <adjipratama22@gmail.com>
2018-11-12 10:39:26 +0700

    download

---
+++ views/download/_search.php
@@ -0,0 +1,93 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+use app\models\Perangkat;
+use app\models\Data;
+use yii\helpers\ArrayHelper;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\DownloadSearch */
+/* @var $form yii\widgets\ActiveForm */
+if (Yii::$app->user->identity->role =='admin') {
+    $perangkats = ArrayHelper::map(Perangkat::find()->all(),'id','id');
+}elseif (Yii::$app->user->identity->role =='user') {
+    $perangkats = ArrayHelper::map(Perangkat::find()->where(['id_owner'=>Yii::$app->user->identity->id])->all(),'id','id');
+    $perangkatq= Perangkat::find()->where(['id_owner'=>Yii::$app->user->identity->id])->one();
+    $sql = 'SELECT YEAR(tgl) as tgl FROM data,perangkat,user WHERE perangkat.id_owner = user.id AND perangkat.id = data.id_perangkat    AND user.id = "'.Yii::$app->user->identity->id.'" GROUP BY YEAR(tgl) ORDER BY YEAR(tgl) DESC';
+    $years = ArrayHelper::map(Data::findBySql($sql)->all(),'tgl','tgl');
+
+    $months =array(
+          '01' => 'Januari' ,
+          '02' => 'Febuari' ,
+          '03' => 'Maret' ,
+          '04' => 'April' ,
+          '05' => 'Mei' ,
+          '06' => 'Juni' ,
+          '07' => 'Juli' ,
+          '08' => 'Agustus' ,
+          '09' => 'September' ,
+          '10' => 'Oktober' ,
+          '11' => 'November' ,
+          '12' => 'Desember' ,
+        );
+}
+
+?>
+
+<div class="data-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+        <?= $form->field($model, 'id_perangkat')->widget(Select2::classname(), [
+            'name' => 'id-perangkat',
+            'id' => 'id-perangkat',
+            'data' => $perangkats,
+            'pluginOptions' => [
+            'placeholder' => 'Pilih perangkat',
+
+            ],
+            'options' => [
+            'onchange'=>'this.form.submit()',
+            ]
+
+            ])->label(false) ?>
+            <?= $form->field($model, 'bulan')->widget(Select2::classname(), [
+                'name' => 'bulan',
+                'id' => 'bulan',
+                'data' => $months,
+                'pluginOptions' => [
+                'placeholder' => 'Pilih bulan',
+                'clearBtn' => true,
+
+                ],
+                'options' => [
+                'onchange'=>'this.form.submit()',
+                ]
+
+                    ])->label(false) ?>
+        <?= $form->field($model, 'tahun')->widget(Select2::classname(), [
+            'name' => 'tahun',
+            'id' => 'tahun',
+            'data' => $years,
+            'pluginOptions' => [
+            'placeholder' => 'Pilih tahun',
+            'clearBtn' => true,
+
+            ],
+            'options' => [
+            'onchange'=>'this.form.submit()',
+            ]
+
+            ])->label(false) ?>
+
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/download/index.php
@@ -0,0 +1,86 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+use kartik\export\ExportMenu;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\DownloadSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Download Data';
+$this->params['breadcrumbs'][] = $this->title;
+$kolom=[
+    ['class' => 'yii\grid\SerialColumn'],
+
+    //'id_data',
+    'id_perangkat',
+    'tgl',
+    'temperature',
+    'kelembaban',
+    'kecepatan_angin',
+    'arah_angin',
+    'curah_hujan',
+    'tekanan_udara',
+];
+?>
+<div class="data-index">
+  <div class="box box-info">
+      <div class="box-body">
+    <?php Pjax::begin(); ?>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?php
+      echo ExportMenu::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $kolom,
+        'target'=> '_blank',
+        'dropdownOptions' => [
+            'label' => 'Download',
+            'class' => 'btn btn-success',
+            'style' => 'border-radius:0;color:white;'
+          ],
+        'columnSelectorOptions' => [
+          'disabled' => true,
+          'label' => 'Kolom',
+          'class' => 'btn',
+          'style' => 'visibility: hidden;width:0;height:0;position:absolute;'
+        ],
+        'exportConfig' => [
+          ExportMenu::FORMAT_HTML => false,
+          ExportMenu::FORMAT_CSV => [
+                'alertMsg' => 'Data akan di export menjadi file CSV',
+            ],
+          ExportMenu::FORMAT_TEXT => [
+                'alertMsg' => 'Data akan di export menjadi file TEXT',
+            ],
+          ExportMenu::FORMAT_PDF => [
+                'alertMsg' => 'Data akan di export menjadi file PDF',
+            ],
+          ExportMenu::FORMAT_EXCEL => [
+                'alertMsg' => 'Data di export menjadi file EXCEL 95+',
+            ],
+          ExportMenu::FORMAT_EXCEL_X => [
+                'alertMsg' => 'Data di export menjadi file EXCEL 2007+',
+            ],
+          ],
+        'filename' => date('YmdHis', mktime(date('H')+5)).'_WSData',
+        'messages' => [
+          'allowPopups' =>  '',
+          'confirmDownload' => 'Lanjutkan proses export ?',
+          'downloadProgress' => 'Memproses file. silahkan tunggu...',
+          'downloadComplete' => 'Download selesai.'
+        ]
+      ]);
+       ?>
+       <br><br>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'summary' => "Preview. Jumlah data : <b id='totaldata'>{totalCount}</b> ",
+        'emptyText' => '<center class="text-danger">Tidak Ada Data, silahkan pilih perangkat, bulan, dan tahun</center>',
+        'columns' => $kolom
+    ]); ?>
+    <?php Pjax::end(); ?>
+  </div></div>
+</div>
