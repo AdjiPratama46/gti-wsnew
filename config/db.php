3a860affc776b9dd018e095afdfbbd9063a7f8a4
PanduMA <pandumuhamad4@gmail.com>
2018-12-04 10:23:21 +0700

    db

--- config/db.php
+++ config/db.php
@@ -5,6 +5,9 @@ return [
     'dsn' => 'mysql:host=stmiot-tech.xyz;dbname=stmiotte_gti',
     'username' => 'stmiotte_gti',
     'password' => 'sk~I-m^myGo&',
+    // 'dsn' => 'mysql:host=localhost;dbname=stmiotte_gti',
+    // 'username' => 'root',
+    // 'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
